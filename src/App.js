1612042ecb9f37a9cc0f1028be7b3d55d1e20f16
Emilia188 <emiliaadasiewicz@gmail.com>
2024-12-12 12:24:21 +0100

    Added search engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
+import Dictionary from "./Dictionary.js";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello</h1>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <h1>Dictionary</h1>
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded by Emilia</footer>
+      </div>
     </div>
   );
 }
