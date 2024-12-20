31db304c591d16ad96a5409dfd1ee8670ff5a296
Emilia188 <emiliaadasiewicz@gmail.com>
2024-12-20 12:29:56 +0100

    Added photo functionality and 2nd api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,23 +1,34 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results.js";
+import Photos from "./Photos.js";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data);
+  }
+  function handlePhotosResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search() {
     // documentaion: https://www.shecodes.io/learn/apis/dictionary
     let apiKey = "a93d1e9t3900f64c3459obcda72aab15";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    //Photos documentation: https://www.shecodes.io/learn/apis/images
+    let photosApiKey = "a93d1e9t3900f64c3459obcda72aab15";
+    let photosApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${photosApiKey}`;
+    axios.get(photosApiUrl).then(handlePhotosResponse);
   }
+
   function handleSubmit(event) {
     event.preventDefault();
     search();
@@ -43,6 +54,7 @@ export default function Dictionary(props) {
           </form>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
